4d6123e91910634eb8bfcd5867367b55ba5d7940
cclcr21 <cclcr21@rit.edu>
2022-05-21 11:58:37 -0400

    dirty change to take terminal params

--- src/Docsharp/Program.cs
+++ src/Docsharp/Program.cs
@@ -17,9 +17,14 @@ namespace Docsharp
         static void Main(string[] args)
         {
             var docs = Docsharpener.From(
-                dllPath: @"C:\Dev\Sharpocs\test\Docsharp.Test.Data\bin\Debug\net5.0\Docsharp.Test.Data.dll",
-                xmlPath: @"C:\Dev\Sharpocs\test\Docsharp.Test.Data\bin\Debug\net5.0\Docsharp.Test.Data.xml"
+                dllPath: args[0],
+                xmlPath: args[1]
             );
+
+            //var docs = Docsharpener.From(
+            //    dllPath: @"C:\Dev\Sharpocs\test\Docsharp.Test.Data\bin\Debug\net5.0\Docsharp.Test.Data.dll",
+            //    xmlPath: @"C:\Dev\Sharpocs\test\Docsharp.Test.Data\bin\Debug\net5.0\Docsharp.Test.Data.xml"
+            //);
 
             docs.Save();
         }
