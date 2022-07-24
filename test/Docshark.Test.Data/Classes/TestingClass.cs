dc4d32446601d4b7c26abb9771a2dc6a80267af7
Chase William <cxr6988@g.rit.edu>
2022-07-24 13:56:51 -0400

    + test data for argumented typed types

---
+++ test/Docshark.Test.Data/Classes/TestingClass.cs
@@ -0,0 +1,18 @@
+﻿using LoxSmoke.DocXml;
+using System;
+using System.Collections.Generic;
+
+namespace Docshark.Test.Data.Classes
+{
+    public class TestingClass
+    {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "myProperty")]
+        public int MyProperty { get; set; }
+
+        public CommonComments Test { get; set; }
+
+        public Action<string> SingleTypedType { get; set; }
+        public Action<string, TestingClass, int, Boat> FourTypeArgumentType { get; set; }
+        public Action<string, TestingClass, int, Action<double, string>> FourTypeNestedArgumentType { get; set; }
+    }
+}
