01c0587f66594ca46f15e2438c7b660bfa23f9ee
Chase William <cxr6988@g.rit.edu>
2022-07-30 18:19:02 -0400

    + extremetly argumented class type for testing

---
+++ test/Docshark.Test.Data/Classes/ArgumentedClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Docshark.Test.Data.Classes
+{
+    public class FromArgumentedClasses : ArgumentedBaseClass<LeftArgument, RightBaseArgument<LeftArgumentLeftArgument, RightArgumentRightArgument>>
+    {
+
+    }
+
+    public class LeftArgument : LeftBaseArgument
+    {
+
+    }  
+
+    public class ArgumentedBaseClass<T, K> : ArgumentedSuperBaseClass
+        where T : LeftBaseArgument
+        where K : RightBaseArgument<LeftArgumentLeftArgument, RightArgumentRightArgument>
+    {
+
+    }
+
+    public class LeftBaseArgument
+    {
+
+    }
+
+    public class RightBaseArgument<T, K>
+        where T : LeftArgumentLeftArgument
+        where K : RightArgumentRightArgument
+    {
+
+    }
+
+    public class RightArgumentRightArgument
+    {
+
+    }
+
+    public class LeftArgumentLeftArgument
+    {
+
+    }
+
+    public class ArgumentedSuperBaseClass
+    {
+
+    }
+}
